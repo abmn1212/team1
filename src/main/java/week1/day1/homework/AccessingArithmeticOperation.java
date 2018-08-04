c71539a88c689a27502a354a3a007da6eac066b3
Abhijit <abmn121294@gmail.com>
2018-08-04 17:56:21 +0530

    First commit

---
+++ src/main/java/week1/day1/homework/AccessingArithmeticOperation.java
@@ -0,0 +1,16 @@
+package week1.day1.homework;
+
+public class AccessingArithmeticOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArithmeticOperation accessobj=new ArithmeticOperation();
+		accessobj.addTwoNumbers();
+		accessobj.substract();
+		System.out.println(accessobj.D);
+		accessobj.multiply();
+		accessobj.divideNumberTwo();
+		
+	}
+
+}
